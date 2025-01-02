850a892d9f6c9383fa021d2b3cb27fada311168f
Ritabrata Patra <teamritabrata@gmail.com>
2025-01-02 23:01:06 +0530

    added image to the card

--- src/components/CardCatalog.tsx
+++ src/components/CardCatalog.tsx
@@ -7,11 +7,14 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Image from "next/image";
 
 const CardCatalog = () => {
   return (
 
-      <Card className="shadow-md    ">
+      <Card className="shadow-md flex items-center">
+        <Image src={"https://picsum.photos/200/300?grayscale"} alt="Product Image" width={200} height={100} className="m-2 rounded-lg"/>
+        <div>
         <CardHeader>
           <CardTitle>Product Title</CardTitle>
           <CardDescription className="text-xs">Product Category</CardDescription>
@@ -22,6 +25,8 @@ const CardCatalog = () => {
         <CardFooter className="text-sm text-neutral-400">
           <p>Product Footer Details like createdAt etc..</p>
         </CardFooter>
+        </div>
+       
       </Card>
 
   );
